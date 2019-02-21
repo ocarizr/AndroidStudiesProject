113483d8764cded95bb3330d10b8c7a663340869
ocarizr <rafael.ocariz90@gmail.com>
2019-02-21 15:28:30 -0300

    improved the work with Images

--- app/src/main/java/com/ocarizr/androidstudiesproject/ImageActivity.java
+++ app/src/main/java/com/ocarizr/androidstudiesproject/ImageActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,12 +16,21 @@ public class ImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image);
 
         final Button btnBackImage = findViewById(R.id.btnBackImage);
+        final ImageButton imgBtnTest = findViewById(R.id.imgBtnTest);
         final ImageView imgAndroidIcon = findViewById(R.id.imgAndroidIcon);
 
         btnBackImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        imgBtnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(ImageActivity.this, getResources().getString(R.string.txtToastTest), Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
 
